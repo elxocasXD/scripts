d006dd652b1bc88be6fae1ee4438bdd563af57f0
elxocasXD <115422799+elxocasXD@users.noreply.github.com>
2023-02-28 19:07:18 -0500

    Update Status.lua

--- Status.lua
+++ Status.lua
@@ -1,7 +1,7 @@
 --Status Gui
 
-getgenv().status = "Dont Work"
-getgenv().Color = Color3.fromRGB(255, 0, 0) --green / Color3.fromRGB(255, 0, 0) red
+getgenv().status = "Works"
+getgenv().Color = Color3.fromRGB(0, 255, 0) --green / Color3.fromRGB(255, 0, 0) red
 getgenv().version = "3.9"
 
 
