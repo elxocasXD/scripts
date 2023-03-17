5b425c4f60aa4b1bb572fa87d18dae7d78cf648b
elxocasXD <115422799+elxocasXD@users.noreply.github.com>
2023-03-16 20:03:08 -0500

    Update Status.lua

--- Status.lua
+++ Status.lua
@@ -1,13 +1,13 @@
 --Status Gui
 
-getgenv().status = "Dont Work"
-getgenv().Color = Color3.fromRGB(255, 0, 0) --green / Color3.fromRGB(255, 0, 0) red
-getgenv().version = "3.9 to 4.0"
+getgenv().status = "Works"
+getgenv().Color = Color3.fromRGB(0, 255, 0) --green / Color3.fromRGB(255, 0, 0) red
+getgenv().version = "4.0"
 
 
 --Trip Hub Status 
 
-getgenv().Trip_Hub_Status = false
+getgenv().Trip_Hub_Status = true
 
 
 --Information
